4cd1e356c23d0daeb12f2b5a76a96e4654d656c6
cervere <bhargav.rrv@gmail.com>
2025-08-08 15:26:22 +0200

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/happywrap.github.io/',
+  base: 'https:/happy-wrap.github.io/',
   server: {
     host: "::",
     port: 8080,
